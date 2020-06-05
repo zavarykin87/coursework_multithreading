8571e5ac56eb5f190c786011ed9956408512faee
Andrew Zavarykin <zavarykin87@gmail.com>
2020-06-05 05:38:27 +0300

    доработки в Server

--- src/ru/zavarykin/multithreading/Server.java
+++ src/ru/zavarykin/multithreading/Server.java
@@ -3,6 +3,11 @@ package ru.zavarykin.multithreading;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 /*Сервер
 Все соединеия необходимо хранить в мапе или сете (потокобезопасных) В ситуации, когда клиент отсоединяется от сервера,
 его соединение необходимо удалять из мапы / сета.
@@ -17,34 +22,64 @@ import java.net.Socket;
 
 public class Server {
     private int port;
-    private Connection connection;
+    private ConcurrentHashMap<Integer, SomeServer> connections = new ConcurrentHashMap<>(); //
+
 
     public Server(int port) {
         this.port = port;
     }
 
-    public void start() throws IOException, ClassNotFoundException {
-        try (ServerSocket serverSocket = new ServerSocket(port)){
-            System.out.println("Сервер запущен...");
-            while (true){
 
-                Socket clientSocket = serverSocket.accept();
-                connection = new Connection(clientSocket);
-                System.out.println(connection.readMessage());
-                connection.sendMessage(Message
-                        .getInstance("server", "hello"));
+    // запускаем сервер и считываем сообщения клиентов
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Сервер начал работу..." + Thread.currentThread().getName());
+        int keyMap = 0;
+        while (true) {
+            Socket clientSocket = serverSocket.accept();   // ждем соединение с клиентом
+            System.out.println("ждем подключения " + Thread.currentThread().getName());
+            connections.put(keyMap, new SomeServer(clientSocket)); // добавляем новое соединение в коллекцию (создается параллельный поток который слушает каждое соединение)
+            System.out.println("подключился - " + connections.get(keyMap));
+            keyMap++;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(8099);
+        server.start();
+
+    }
+
+
+}
+// принимает сообщение от клиента и выводит в консоле сервера
+class SomeServer extends Thread {
+    private Socket clientSocket;
+    private Connection connection;
+    private Message fromClient;
+
+    public Message getFromClient() {
+        return fromClient;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public SomeServer(Socket clientSocket) throws IOException {
+        this.clientSocket = clientSocket;
+        connection = new Connection(clientSocket);
+        start();
+    }
+    @Override
+    public void run() {
+        while (true){
+            try {
+                fromClient = connection.readMessage();
+                System.out.println(fromClient + Thread.currentThread().getName());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
-
-    public static void main(String[] args) {
-        int port = 8099;
-        Server server = new Server(port);
-        try {
-            server.start();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
