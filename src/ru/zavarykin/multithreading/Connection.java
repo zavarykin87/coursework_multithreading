7a3799f9f066625ad20b9a402e41c641955ae530
Andrew Zavarykin <zavarykin87@gmail.com>
2020-06-06 20:36:16 +0300

    добавил в Connection обработку исключений и закрытие потоков

--- src/ru/zavarykin/multithreading/Connection.java
+++ src/ru/zavarykin/multithreading/Connection.java
@@ -5,34 +5,46 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Connection implements AutoCloseable{
+public class Connection {
 
-        private Socket socket;
-        private ObjectOutputStream output;
-        private ObjectInputStream input;
+    private Socket socket;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
 
-        public Connection(Socket socket) throws IOException {
-            this.socket = socket;
-            output = new ObjectOutputStream(
-                    this.socket.getOutputStream());
-            input = new ObjectInputStream(
-                    this.socket.getInputStream());
-        }
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        output = new ObjectOutputStream(this.socket.getOutputStream());
+        input = new ObjectInputStream(this.socket.getInputStream());
+    }
 
-        public void sendMessage(Message message) throws IOException {
-            message.setDateTime();
+    public void sendMessage(Message message) {
+        message.setDateTime();
+        try {
             output.writeObject(message);
             output.flush();
+        } catch (IOException e) {
+            close();
         }
+    }
 
-        public Message readMessage() throws IOException, ClassNotFoundException {
-            return (Message) input.readObject();
+    public Message readMessage() {
+        Message message = null;
+        try {
+            message = (Message) input.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            close();
         }
+        return message;
+    }
 
-        @Override
-        public void close() throws Exception {
+    public void close() {
+        try {
             input.close();
             output.close();
             socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+}
